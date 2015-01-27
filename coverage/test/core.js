7e05eab7a3991c150334c8a7b57ad0d1463cac92
loki <loki951753@gmail.com>
2015-01-27 17:06:16 +0800

    init

---
+++ coverage/test/core.js
@@ -0,0 +1,31 @@
+module("core");
+
+test("Basic requirements", function() {
+	expect(7);
+	ok( Array.prototype.push, "Array.push()" );
+	ok( Function.prototype.apply, "Function.apply()" );
+	ok( document.getElementById, "getElementById" );
+	ok( document.getElementsByTagName, "getElementsByTagName" );
+	ok( RegExp, "RegExp" );
+	ok( jQuery, "jQuery" );
+	ok( $, "$" );
+});
+
+test("jQuery.camelCase()", function() {
+
+	var tests = {
+		"foo-bar": "fooBar",
+		"foo-bar-baz": "fooBarBaz",
+		"girl-u-want": "girlUWant",
+		"the-4th-dimension": "the4thDimension",
+		"-o-tannenbaum": "OTannenbaum",
+		"-moz-illa": "MozIlla",
+		"-ms-take": "msTake"
+	};
+
+	expect(7);
+
+	jQuery.each( tests, function( key, val ) {
+		equal( jQuery.camelCase( key ), val, "Converts: " + key + " => " + val );
+	});
+});
